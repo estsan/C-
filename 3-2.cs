2cbebdb271d3af2b54dea522881f57371882a25d
estsan <42601915+estsan@users.noreply.github.com>
2018-08-27 11:59:16 +0200

    Create 3-2.cs

---
+++ 3-2.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Globalization.CultureInfo.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+
+            Console.Write("Fakuliteten av vaddå: ");
+            int n = int.Parse(Console.ReadLine());
+            int p = 1;
+
+            for (int i = 1; i < n+1; i++)
+            {
+                p = p * i;
+            }
+            Console.WriteLine("Fakuliteten av " + n + " är " + p);
+            Console.ReadLine();
+
+        }
+    }
+}
