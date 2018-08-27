5ffcfa70c150b1e9a057921374a3e2c356125213
estsan <42601915+estsan@users.noreply.github.com>
2018-08-27 12:55:02 +0200

    Create 3-6.cs

---
+++ 3-6.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Globalization.CultureInfo.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+
+            double vetekorn = 0;
+
+            // Antal rutor
+            int max = 4;
+
+            for (int i = 0; i < max; i++)
+            {
+                vetekorn = vetekorn + Math.Pow(2,i);
+            }
+            Console.WriteLine(vetekorn);
+            Console.ReadLine();
+
+        }
+    }
+}
