387ffeefc966b32c1c3846d74792867f5d70c39c
estsan <42601915+estsan@users.noreply.github.com>
2018-08-27 12:04:08 +0200

    Create 3-3.cs

---
+++ 3-3.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Globalization.CultureInfo.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+
+            Console.Write("Ge mig ett tal: ");
+            int n = int.Parse(Console.ReadLine());
+            int sj = 0;
+            int su = 0;
+
+            for (int i = 2; i < n+1; i += 2)
+            {
+                sj = sj + i;
+            }
+
+            for (int i = 1; i < n + 1; i += 2)
+            {
+                su = su + i;
+            }
+            Console.WriteLine("Summan av alla jämna tal upp till " + n + " är " + sj + " och summan av alla udda är " + su);
+            Console.ReadLine();
+
+        }
+    }
+}
