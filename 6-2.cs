028d9d43ca402fa707008755e8a38373f049e1d8
estsan <42601915+estsan@users.noreply.github.com>
2018-08-28 10:42:56 +0200

    Create 6-2.cs

---
+++ 6-2.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Globalization.CultureInfo.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+
+            Console.Write("Text plax: ");
+            string forward = Console.ReadLine();
+
+            int n = forward.Length;
+
+            Console.Write("Baklänges: ");
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(forward[n - i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
