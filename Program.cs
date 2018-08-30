3a3026f6506157d412ef3817b49726008d3238fc
estsan <42601915+estsan@users.noreply.github.com>
2018-08-30 15:32:45 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.GetEncoding("ISO-8859-1");
 
             // Läs in en mening från användaren
             Console.WriteLine("Hit me up fam, I will disclose the Swedishness of your sentence!");
             string text = Console.ReadLine();
 
-            string lowerText = text.ToLower();
-                
             // Kolla hur lång meningen är
-            int n = lowerText.Length;
+            int n = text.Length;
             // Ansätt några variabler
             int i;
             int aacount = 0;
@@ -15,17 +26,17 @@
 
             // Hur många å:n finns i meningen?
             for (i = 0; i < n; i++)
-                if (lowerText[i] == char.Parse("å")) // || text[i] == char.Parse("Å"))
+                if (text[i] == char.Parse("å") || text[i] == char.Parse("Å"))
                     aacount++;
 
             //Hur många ä:n finns i meningen?
             for (i = 0; i < n; i++)
-                if (lowerText[i] == char.Parse("ä")) // || text[i] == char.Parse("Ä"))
+                if (text[i] == char.Parse("ä") || text[i] == char.Parse("Ä"))
                     aecount++;
 
             // Hur många ö:n finns det i meningen?
             for (i = 0; i < n; i++)
-                if (lowerText[i] == char.Parse("ö")) // || text[i] == char.Parse("Ö"))
+                if (text[i] == char.Parse("ö") || text[i] == char.Parse("Ö"))
                     oecount++;
 
             // Är antalet å, ä och ö noll?
@@ -33,18 +44,19 @@
             bool aabool = aacount == 0;
             bool aebool = aecount == 0;
             bool oebool = oecount == 0;
+            int sum = aacount + aecount + oecount;
 
             // Om det inte finns å, ä, eller ö så tror vi att texten inte är svenska, annar är det antagligen det.            
             if (aabool && aebool && oebool)
-                Console.WriteLine("THIS IS (probably) NOT SWEDISH YOU FOOL");
+                Console.WriteLine("THIS IS (probably) NOT SWEDISH YOU FOOL, 0 SWEDISH LETTERS!");
             else
-                Console.WriteLine("Svenska it is! Väl?");
+                Console.WriteLine("Svenska it is! Väl? Den har ändå " + sum + " svenska bokstäver");
 
             // Printa antalet å, ä och ö.
             Console.WriteLine("Antal å: " + aacount + ", antal ä: " + aecount + ", och antal ö: " + oecount);
 
             // En lite överraskning för att testa Sleep.
-            System.Threading.Thread.Sleep(5000);
+            System.Threading.Thread.Sleep(4000);
             Console.Write(".");
             System.Threading.Thread.Sleep(500);
             Console.Write("o");
@@ -59,4 +71,7 @@
             System.Threading.Thread.Sleep(500);
             Console.Write(".");
             Console.ReadKey();
+        }
+    }
+}
 
