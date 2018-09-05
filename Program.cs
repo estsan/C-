55929f066b976925d43f26e9b2241d32a665ba38
estsan <42601915+estsan@users.noreply.github.com>
2018-09-05 12:15:46 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,66 +12,46 @@ namespace ConsoleApp1
         {
             Console.InputEncoding = Encoding.GetEncoding("ISO-8859-1");
 
-            // Läs in en mening från användaren
-            Console.WriteLine("Hit me up fam, I will disclose the Swedishness of your sentence!");
+            Console.WriteLine("Hit me up fam, I will disclose the Swedishness of your sentence!");  // Läs in en mening från användaren
             string text = Console.ReadLine();
-
-            // Kolla hur lång meningen är
-            int n = text.Length;
-            // Ansätt några variabler
-            int i;
-            int aacount = 0;
+            int aacount = 0;                  // Ansätt några variabler
             int aecount = 0;
             int oecount = 0;
 
-            // Hur många å:n finns i meningen?
-            for (i = 0; i < n; i++)
-                if (text[i] == char.Parse("å") || text[i] == char.Parse("Å"))
+            foreach (char c in text)
+            {
+                if (c == 'å' || c == 'Å')        // Hur många å:n finns i meningen?he 
+                {  
                     aacount++;
+                }
 
-            //Hur många ä:n finns i meningen?
-            for (i = 0; i < n; i++)
-                if (text[i] == char.Parse("ä") || text[i] == char.Parse("Ä"))
-                    aecount++;
+                if (c == 'ä' || c == 'Ä')        // Hur många ä:n finns i meningen?
+                {
+                    aacount++;
+                }
 
-            // Hur många ö:n finns det i meningen?
-            for (i = 0; i < n; i++)
-                if (text[i] == char.Parse("ö") || text[i] == char.Parse("Ö"))
-                    oecount++;
-
-            // Är antalet å, ä och ö noll?
+                if (c == 'ö' || c == 'Ö')        // Hur många ö:n finns i meningen?
+                {
+                    aacount++;
+                }
+            }
 
             bool aabool = aacount == 0;
             bool aebool = aecount == 0;
             bool oebool = oecount == 0;
-            int sum = aacount + aecount + oecount;
+            int sum = aacount + aecount + oecount;     // Är antalet å, ä och ö noll?
 
-            // Om det inte finns å, ä, eller ö så tror vi att texten inte är svenska, annar är det antagligen det.            
-            if (aabool && aebool && oebool)
-                Console.WriteLine("THIS IS (probably) NOT SWEDISH YOU FOOL, 0 SWEDISH LETTERS!");
+            if (aabool && aebool && oebool)            // Om det inte finns å, ä, eller ö så tror vi att texten inte är svenska    
+            {
+                Console.Write("THIS IS (probably) NOT SWEDISH YOU FOOL, 0 SWEDISH LETTERS! ");
+            }
             else
-                Console.WriteLine("Svenska it is! Väl? Den har ändå " + sum + " svenska bokstäver");
+            {                                     //annars är det antagligen det.        
+                Console.Write("Svenska it is! Väl? Den har ändå " + sum + " svenska bokstäver: ");
+            }
 
-            // Printa antalet å, ä och ö.
-            Console.WriteLine("Antal å: " + aacount + ", antal ä: " + aecount + ", och antal ö: " + oecount);
-
-            // En lite överraskning för att testa Sleep.
-            System.Threading.Thread.Sleep(4000);
-            Console.Write(".");
-            System.Threading.Thread.Sleep(500);
-            Console.Write("o");
-            System.Threading.Thread.Sleep(500);
-            Console.Write("0");
-            System.Threading.Thread.Sleep(500);
-            Console.Write("O");
-            System.Threading.Thread.Sleep(500);
-            Console.Write("0");
-            System.Threading.Thread.Sleep(500);
-            Console.Write("o");
-            System.Threading.Thread.Sleep(500);
-            Console.Write(".");
+            Console.WriteLine(aacount + " st å:n, " + aecount + " st ä:n, " + oecount + " och st ö:n");  // Printa antalet å, ä och ö.
             Console.ReadKey();
         }
     }
 }
-
