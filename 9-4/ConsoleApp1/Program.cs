eabdcd0c3c281454462239ea075206abd63c3cf1
Unknown <sandstrom.ester@gmail.com>
2018-09-16 18:22:19 +0200

    9-4
    Lösning till uppgift 4, lektion 9

---
+++ 9-4/ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class IntList
+    {
+        int[] internalArray = new int[10];
+        int Length = 0;
+
+        public void Add(int x)
+        {
+            if (internalArray.Length == Length)
+            {
+                int[] internalArrayTemp = new int[Length + 10];
+                for (int i = 0; i < Length; i++)
+                {
+                    internalArrayTemp[i] = internalArray[i];
+                }
+                internalArray = internalArrayTemp;
+            }
+            internalArray[Length] = x;
+            Length++;
+        }
+        public void Remove(int y)
+        {
+            int indexOfRemove = 0;
+            for (int i = 0; i < Length; i++)
+            {
+                if (internalArray[i] == y)
+                {
+                    indexOfRemove = i;
+                    Length--;
+                    break;
+                }
+            }
+            for (int i = indexOfRemove; i < Length; i++)
+            {
+                internalArray[i] = internalArray[i + 1];
+            }
+        }
+    
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntList lista = new IntList();
+
+            lista.Add(1);
+            lista.Add(10);
+            lista.Add(3);
+
+            lista.Remove(1);
+            lista.Remove(3);
+
+            IntList listo = new IntList();
+            listo.Add(3);
+
+            lista.Remove(3);
+
+
+        }
+    }
+}
