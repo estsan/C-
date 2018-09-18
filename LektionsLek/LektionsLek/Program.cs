da651b322bf8c40404e07234580ebe6b7f5b8dda
Unknown <sandstrom.ester@gmail.com>
2018-09-18 10:20:00 +0200

    i

--- LektionsLek/LektionsLek/Program.cs
+++ LektionsLek/LektionsLek/Program.cs
@@ -4,57 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Session9
+namespace ConsoleApp1
 {
-    class Person
-    {
-        public string Name;
-        public int Age;
-        public int Fortune;
-        public int Salary; //per month
-
-        public Person PaySalary()
-        {
-            Fortune += Salary;
-            return this;
-        }
-        public Person CelebrateBirthday()
-        {
-            Age++;
-            Fortune += 500;
-            return this;
-        }
-        public Person WinLottery(int prize)
-        {
-            Fortune += prize;
-            return this;
-        }
-        public string Show()
-        {
-            string info = "";
-            info = Age + " year old " + Name + " has a salary of " + Salary + " swedish krona and " + Fortune + " swedish krona in the bank.";
-            return info;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Personens namn, ålder tillgångar och månadslön: ");
-            Person p1 = new Person
+            Console.Write("Please enter a number: ");
+            try
             {
-                Name = Console.ReadLine(),
-                Age = int.Parse(Console.ReadLine()),
-                Fortune = int.Parse(Console.ReadLine()),
-                Salary = int.Parse(Console.ReadLine())
-
-            };
-
-            p1.PaySalary();
-            p1.CelebrateBirthday();
-            p1.WinLottery(2000);
-            p1.Show();
-            Console.Write("{0} är {1} år gammal, har {2} kr, och tjänar {3} varje månad", p1.Name, p1.Age, p1.Fortune, p1.Salary);
+                int number = int.Parse(Console.ReadLine());
+                int result = 10 / number;
+                Console.WriteLine("10 divided by your number is: " + result);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("You did not enter a number.");
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("Division by 0 is not allowed!");
+            }
             Console.ReadKey();
         }
     }
