2500f8efb3d165ea260e5c04ec3138a48ebc5dc0
Unknown <sandstrom.ester@gmail.com>
2018-09-18 10:19:53 +0200

    1-5

--- 1-5/1-5/Program.cs
+++ 1-5/1-5/Program.cs
@@ -10,17 +10,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Skriv 4 koordinater, x1, y1, x2 och y2, så ska vi se om jag kan gissa avståndet mellan punkterna");
-            double x1 = double.Parse(Console.ReadLine());
-            double y1 = double.Parse(Console.ReadLine());
-            double x2 = double.Parse(Console.ReadLine());
-            double y2 = double.Parse(Console.ReadLine());
-
-            double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
-
-            Console.WriteLine("Avståndet torde vara " + d + ", gissningsvis.");
-            Console.WriteLine("Var det rätt? Noice! Klicka enter nu och fortsätt knacka kod");
-            Console.ReadLine();
+            Console.WriteLine("Please enter your name");
+            int age = int.Parse(Console.ReadLine());
+            int newAge = age + 1;
+            Console.WriteLine("Next year you will be " + newAge + " years old");
         }
     }
 }
