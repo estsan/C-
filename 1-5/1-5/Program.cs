ab771fdd05a78eedeaa3aab5f30eb9c6e2e283e7
Unknown <sandstrom.ester@gmail.com>
2018-09-18 09:35:14 +0200

    1-5
    Lektion 1, uppgift 5

---
+++ 1-5/1-5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv 4 koordinater, x1, y1, x2 och y2, så ska vi se om jag kan gissa avståndet mellan punkterna");
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+
+            Console.WriteLine("Avståndet torde vara " + d + ", gissningsvis.");
+            Console.WriteLine("Var det rätt? Noice! Klicka enter nu och fortsätt knacka kod");
+            Console.ReadLine();
+        }
+    }
+}
