cdc3b41c5a706311064485124d1e39b417270f1d
Unknown <sandstrom.ester@gmail.com>
2018-10-04 10:07:01 +0200

    GUI-lektioner

---
+++ LektionsPloj/LektionsPloj/MyForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace LektionsPloj
+{
+    class MyForm : Form
+    {
+        private TextBox nameBox;
+
+        public MyForm()
+        {
+            TableLayoutPanel table = new TableLayoutPanel
+            {
+                RowCount = 2,
+                ColumnCount = 2
+
+            };
+            Controls.Add(table);
+
+            nameBox = new TextBox { };
+            table.Controls.Add(nameBox);
+
+            Button button1 = new Button
+            {
+                Text = "Try to insult me",
+            };
+            table.Controls.Add(button1);
+
+            button1.Click += button1_Click;
+            // När Click sker på knappen, anropa metoden "button1_Click"
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You can be " + nameBox.Text);
+        }
+    }
+}
