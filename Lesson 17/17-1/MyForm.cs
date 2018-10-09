5b82f31e4a81b1aece46b1bce08777f754dc81df
Unknown <sandstrom.ester@gmail.com>
2018-10-09 11:02:16 +0200

    Nöjd,för utveckling gör snyggare

--- Lesson 17/17-1/MyForm.cs
+++ Lesson 17/17-1/MyForm.cs
@@ -9,15 +9,23 @@ using System.IO;
 
 namespace _17_1
 {
+    class Barbershopper
+    {
+        public string Name;
+        public string Part;
+        public string Summary;
+    }
     class MyForm : Form
     {
         #region Instance Variables
         TableLayoutPanel panel;
         ComboBox dropDownMenu;
         Label info;
+        Barbershopper[] barbers;
         string infoString;
         string part;
         string member;
+        string summary;
         string[] ringmastersMembers;
         string[,] ringmastersMemberParts;
         int m;
@@ -33,19 +41,20 @@ namespace _17_1
                 RowCount = 3,
                 Dock = DockStyle.Fill
             };
-            Controls.Add(panel);
             #endregion
 
             #region Things
             Label rubrik = new Label
             {
                 Text = "Hey! Don't spy!",
-                Dock = DockStyle.Fill
+                Dock = DockStyle.Fill,
+                TextAlign = ContentAlignment.MiddleCenter
             };
 
             dropDownMenu = new ComboBox
             {
-
+                DropDownStyle = ComboBoxStyle.DropDownList,
+                Dock = DockStyle.Fill
             };
 
             //Button spy = new Button
@@ -55,31 +64,18 @@ namespace _17_1
             #endregion
 
             #region DropDownMenu
-            panel.Controls.Add(rubrik);
-            ringmastersMembers = File.ReadAllLines("People.txt");
-            m = ringmastersMembers.Length;
-            ringmastersMemberParts = new string[m, 2];
-
-            int i = 0;
-            foreach (string line in ringmastersMembers)
+            barbers = MakeBarbershopper();
+            foreach (Barbershopper b in barbers)
             {
-                string[] temp = line.Split(new char[] { ',' });
-
-                    for (int j = 0; j < 2; j++)
-                    {
-                        ringmastersMemberParts[i, j] = temp[j].Trim(' ');
-                    }
-                i++;
-            }
-            for (int k = 0; k < m; k++)
-            {
-                dropDownMenu.Items.Add(ringmastersMemberParts[k,0]);
+                dropDownMenu.Items.Add(b.Name);
             }
             #endregion
 
             #region Adding to panel
+            Controls.Add(panel);
+            panel.Controls.Add(rubrik);
             panel.Controls.Add(dropDownMenu);
-            dropDownMenu.DropDownClosed += MenuClosed;
+            dropDownMenu.SelectedIndexChanged += MenuClosed;
             //panel.Controls.Add(spy);
             //spy.Click += Info;
             #endregion
@@ -88,9 +84,11 @@ namespace _17_1
         {
             int w = dropDownMenu.SelectedIndex;
 
-            member = ringmastersMemberParts[w, 0];
-            part = ringmastersMemberParts[w, 1];
-            infoString = member + " sings " + part + " in Ringmasters.";
+            member = barbers[w].Name;
+            part = barbers[w].Part;
+            summary = barbers[w].Summary;
+
+            infoString = member + " sings " + part + " in Ringmasters. He is " + summary;
 
             panel.Controls.Remove(info);
 
@@ -102,5 +100,29 @@ namespace _17_1
             info.TextAlign = ContentAlignment.MiddleCenter;
             panel.Controls.Add(info);
         }
+
+        private Barbershopper[] MakeBarbershopper()
+        {
+            ringmastersMembers = File.ReadAllLines("People.txt");
+            m = ringmastersMembers.Length;
+            ringmastersMemberParts = new string[m, 2];
+            Barbershopper[] barbers = new Barbershopper[m];
+            int i = 0;
+
+            foreach (string line in ringmastersMembers)
+            {
+                Barbershopper barb = new Barbershopper { };
+                string[] temp = line.Split(new char[] { ',' });
+                barb.Name = temp[0].Trim(' ');
+                barb.Part = temp[1].Trim(' ');
+                barb.Summary = temp[2].Trim(' ');
+                barbers[i] = barb;
+                i++;
+            }
+            return barbers;
+
+
+        }
+
     }
 }
