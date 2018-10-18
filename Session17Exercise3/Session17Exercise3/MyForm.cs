6493bef15265a0867ee80b5b48307666b61cff06
Unknown <sandstrom.ester@gmail.com>
2018-10-18 15:18:43 +0200

    Lösning

---
+++ Session17Exercise3/Session17Exercise3/MyForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace Session17Exercise3
+{
+    class MyForm : Form
+    {
+        DataGridView dataGridView;
+
+        public MyForm()
+        {
+            dataGridView = new DataGridView
+            {
+                Dock = DockStyle.Fill,
+                ColumnCount = 3,
+            };
+            Controls.Add(dataGridView);
+            dataGridView.Columns[0].Name = "Placering";
+            dataGridView.Columns[1].Name = "Djur";
+            dataGridView.Columns[2].Name = "Sort";
+            PopulateDataGridView();
+        }
+        private void PopulateDataGridView()
+        {
+            string[] rows = File.ReadAllLines("TextFile1.txt");
+            foreach (string line in rows)
+            {
+                string[] listItem = line.Split(new char[] { ',' });
+                dataGridView.Rows.Add(listItem);
+            }
+        }
+    }
+}
