614ea706ddc7b4e8b1626ea05432982cb4a551bd
Unknown <sandstrom.ester@gmail.com>
2018-10-18 15:47:55 +0200

    Lösning

---
+++ Session19Exercise1/Session19Exercise1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session19Exercise1
+{
+    class Person
+    {
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >= 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new Exception("ej negativ ålder");
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person { };
+            p.Age = -5;
+        }
+    }
+}
